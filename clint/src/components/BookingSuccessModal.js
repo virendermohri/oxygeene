ebfedea0933fe0204d50dfdd991f12cc0fb22561
virendermohri <virendermohri@gamil.com>
2025-06-27 12:51:01 +0530

    add '

--- clint/src/components/BookingSuccessModal.js
+++ clint/src/components/BookingSuccessModal.js
@@ -9,7 +9,7 @@ const BookingSuccessModal = ({ isOpen, onClose }) => {
       <div className="bg-white p-6 rounded-lg shadow-md max-w-sm w-full text-center">
         <h2 className="text-xl  flex items-center justify-center gap-2 font-semibold text-green-600 mb-4"><FaCheckSquare className=''/> Booking Confirmed!</h2>
         <p className="text-gray-700 mb-4">
-          Thank you for booking with CareKwik. You'll receive a confirmation message shortly.
+          Thank you for booking with CareKwik. You&sbquo;ll receive a confirmation message shortly.
         </p>
         <button
           onClick={onClose}
