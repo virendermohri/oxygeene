7d0443340090347ec19ddc8f6f95d5da136e4755
virendermohri <virendermohri@gamil.com>
2025-06-27 12:45:55 +0530

    add icon for booking

--- clint/src/components/BookingSuccessModal.js
+++ clint/src/components/BookingSuccessModal.js
@@ -1,13 +1,13 @@
 // components/BookingSuccessModal.jsx
 import React from 'react';
-
+import { FaCheckSquare } from "react-icons/fa";
 const BookingSuccessModal = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+  if (isOpen) return null;
 
   return (
     <div className="fixed inset-0 backdrop-10 p-5  z-50 flex justify-center items-center">
       <div className="bg-white p-6 rounded-lg shadow-md max-w-sm w-full text-center">
-        <h2 className="text-xl font-semibold text-green-600 mb-4">✅ Booking Confirmed!</h2>
+        <h2 className="text-xl  flex items-center justify-center gap-2 font-semibold text-green-600 mb-4"><FaCheckSquare className=''/> Booking Confirmed!</h2>
         <p className="text-gray-700 mb-4">
           Thank you for booking with CareKwik. You'll receive a confirmation message shortly.
         </p>
