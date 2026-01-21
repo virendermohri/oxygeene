95650a6ab76ba1ae346a79747824f14593a43aa2
virendermohri <virendermohri@gamil.com>
2026-01-21 19:52:49 +0530

    add google anyltics

--- clint/src/app/layout.js
+++ clint/src/app/layout.js
@@ -48,6 +48,15 @@ export default function RootLayout({ children }) {
           <Footer />
         </footer>
       </body>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XFCY1RSG1N"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XFCY1RSG1N');
+</script>
     </html>
   );
 }
